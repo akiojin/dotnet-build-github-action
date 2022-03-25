8a978b9d0f62461400d9a85f939561f2bf7a3cac
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-03-25 21:32:31 +0900

    初期コミット

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import { ArgumentBuilder } from '@akiojin/argument-builder'
+
+async function Run(): Promise<void> 
+{
+	try {
+		const builder = new ArgumentBuilder()
+			.Append('build')
+			.Append('--configuration', core.getInput('configuration'))
+			.Append('--output', core.getInput('output'))
+
+		await exec.exec('dotnet', builder.Build())
+	} catch (ex: any) {
+		core.setFailed(ex.message);
+	}
+}
+
+Run()
